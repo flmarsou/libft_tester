5388974846df9c7bf93469143bfc86475c13682d
flmarsou <marsouinflorent12@gmail.com>
2025-03-25 17:09:14 +0100

    Update test_strlen.c

--- src/tests/test_strlen.c
+++ src/tests/test_strlen.c
@@ -2,50 +2,23 @@
 
 size_t	ft_strlen(const char *str);
 
-void	test_strlen(void)
+static void	run_test(const char *input, size_t expected)
 {
 	size_t	result;
 
-	// Basic String
-	result = ft_strlen("Hello World");
-	if (result == 11)
-		printf(SUCESS);
+	result = ft_strlen(input);
+	if (result == expected)
+		printf(SUCCESS);
 	else
-		printf("\n%s - \"Hello World\" returned %lu instead of 8!\n", ERROR, result);
+		printf("\n%s - \"%s\" returned %lu instead of %lu!\n", ERROR, input, result, expected);
+}
 
-	// String with Newline Character
-	result = ft_strlen("Hello\nWorld");
-	if (result == 11)
-		printf(SUCESS);
-	else
-		printf("\n%s - \"Hello\\nWorld\" returned %lu instead of 11!\n", ERROR, result);
-
-	// String with Special Characters
-	result = ft_strlen("Hello!@#$%^&*");
-	if (result == 13)
-		printf(SUCESS);
-	else
-		printf("\n%s - \"Hello!@#$%%^&*\" returned %lu instead of 13!\n", ERROR, result);
-
-	// String with Escape Sequences
-	result = ft_strlen("\a\b\t\n\v\f\r");
-	if (result == 7)
-		printf(SUCESS);
-	else
-		printf("\n%s - \"\\a\\b\\t\\n\\v\\f\\r\" returned %lu instead of 7!\n", ERROR, result);
-
-	// String with Null Character
-	result = ft_strlen("Hello\0World");
-	if (result == 5)
-		printf(SUCESS);
-	else
-		printf("\n%s - \"Hello\\0World\" returned %lu instead of 7!\n", ERROR, result);
-
-		// Empty String
-	result = ft_strlen("");
-	if (result == 0)
-		printf(SUCESS);
-	else
-		printf("\n%s - \"\" returned %lu instead of 0!\n", ERROR, result);
-
+void	test_strlen(void)
+{
+	run_test("Hello World", strlen("Hello World"));		// Basic String
+	run_test("Hello\nWorld", strlen("Hello\nWorld"));	// String with Newline Character
+	run_test("Hello!@#$%^&*", strlen("Hello!@#$%^&*"));	// String with Special Characters
+	run_test("\a\b\t\n\v\f\r", strlen("\a\b\t\n\v\f\r"));	// String with Escape Sequences
+	run_test("Hello\0World", strlen("Hello\0World"));	// String with Null Character
+	run_test("", strlen(""));				// Empty String
 }
