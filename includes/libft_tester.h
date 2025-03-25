908653f9687f1ec5291d100ef087dd52b40f4b5d
flmarsou <marsouinflorent12@gmail.com>
2025-03-25 15:44:40 +0100

    Create libft_tester.h

---
+++ includes/libft_tester.h
@@ -0,0 +1,19 @@
+#ifndef LIBFT_TESTER_H
+# define LIBFT_TESTER_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+
+# define GREEN		"\e[1;32m"
+# define RED		"\e[1;31m"
+# define PURPLE		"\e[1;35m"
+# define RESET		"\e[0m"
+
+# define SUCESS		GREEN"[ OK ] "RESET
+# define ERROR		RED"[ ERROR ]"RESET
+# define WARN		PURPLE"[ WARNING ]"RESET
+
+void	test_strlen(void);
+
+#endif
