b31eee32651716ced14b256865d0d869e695df5e
flmarsou <marsouinflorent12@gmail.com>
2025-03-27 09:52:32 +0100

    Added ctype tests and score

--- includes/libft_tester.h
+++ includes/libft_tester.h
@@ -3,17 +3,31 @@
 
 # include <unistd.h>
 # include <stdio.h>
+# include <stdbool.h>
+# include <ctype.h>
 # include <string.h>
 
+# define BOLD		"\e[1m"
 # define GREEN		"\e[1;32m"
 # define RED		"\e[1;31m"
 # define PURPLE		"\e[1;35m"
 # define RESET		"\e[0m"
 
-# define SUCCESS		GREEN"[ OK ] "RESET
-# define ERROR		RED"[ ERROR ]"RESET
-# define WARN		PURPLE"[ WARNING ]"RESET
+# define SUCCESS	GREEN"[ OK ] "RESET
+# define ERROR		RED"[ KO ] "RESET
+# define WARN		PURPLE"[ WARNING ] "RESET
+
+# define SCORE_MAX	
+
+extern int	score;
 
 void	test_strlen(void);
+void	test_isalnum(void);
+void	test_isalpha(void);
+void	test_isascii(void);
+void	test_isdigit(void);
+void	test_isprint(void);
+void	test_tolower(void);
+void	test_toupper(void);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,35 @@
 #include "libft_tester.h"
 
-int	main()
+int	score = 0;
+
+static void	print_score()
 {
-	test_strlen();
+	if (score < 100)
+	{
+		printf(BOLD"╔═══════════════╗\n"RESET);
+		printf(BOLD"║    %3d/133    ║\n"RESET, score);
+		printf(BOLD"╚═══════════════╝\n"RESET);
+	}
+	else
+	{
+		printf(BOLD"╔═══════════════╗\n"RESET);
+		printf(BOLD"║    %d/133    ║\n"RESET, score);
+		printf(BOLD"╚═══════════════╝\n"RESET);
+	}
+}
+
+int	main(void)
+{
+	printf(BOLD"Functions from <ctype.h>\n"RESET);
+	test_isalnum();	// 7 Tests
+	test_isalpha();	// 7 Tests
+	test_isascii();	// 7 Tests
+	test_isdigit();	// 7 Tests
+	test_isprint();	// 3 Tests
+	test_tolower();	// 1 Tests
+	test_toupper();	// 1 Tests
+
+	print_score();
+
 	return (0);
 }
